5cd9325a568fdc2b626dc43185ff9511800b1fb3
jenny <jchaves071397@gmail.com>
2021-06-14 23:05:08 -0600

    newcommit

--- script.js
+++ script.js
@@ -1,52 +1,54 @@
 function toggleRead() {
     var readMoreContent = document.getElementById("read-more-content");
-    var readMoreContent = document.getElementById("readMore");
     if (readMoreContent.style.display === "block") {
-        readMoreContent.style.display = "none";
-        readMoreButton.textContent = "Leer más...";
-    }  else {
-        readMoreContentElement.style.display = "block";
-        readMoreButton.textContent = "Leer menos...";
+        document.getElementById("read-more-content").style.display = "none";
+        document.getElementById("readMore").textContent = "Leer más...";
+    } else {
+        document.getElementById("read-more-content").style.display = "block";
+        document.getElementById("readMore").textContent = "Leer menos...";
     }
-
+     
 }
 
-function showCategory(type) {
-  var paseosCategory = document.getElementById("paseos");
-  var comidaCategory = document.getElementById("comida");
-  var hobbiesCategory = document.getElementById("hobbies");
+function mostrarCategoria(type) {
+  var comidaElement= document.getElementById("comida");
+  var paseosElement= document.getElementById("paseos");
+  var hobbiesElement= document.getElementById("hobbies");
 
-  var paseosBtnCategory = document.getElementById("paseos-btn");
-  var comidaBtnCategory = document.getElementById("comida-btn");
-  var hobbiesBtnCategory = document.getElementById("hobbies-btn");
-  switch (type) { 
-     case "paseos":
-          comidaCategory.style.display = "none";
-          hobbiesCategory.style.display = "none";
-          paseosCategory.style.display = "block";
+  var comidaBtnElement = document.getElementById("comida-btn");
+  var paseosBtnElement= document.getElementById("paseos-btn");
+  var hobbiesBtnElement= document.getElementById("hobbies-btn");
+  switch (type) {
+      case "comida":
+           paseosElement.style.display = "none";
+           hobbiesElement.style.display = "none";
+           comidaElement.style.display = "block";
 
-          comidaBtnCategory.style.backgroundColor = "#f47c24";
-          hobbiesBtnCategory.style.backgroundColor = "#f47c24";       
-          paseosBtnCategory.style.backgroundColor = "#5a0302";
-          break;
-      case "hobbies":
-          comidaCategory.style.display = "none";
-          paseosCategory.style.display = "none";
-          hobbiesCategory.style.display = "block";
+           paseosBtnElement.style.backgroundColor = "rgb(244, 124, 36)";
+           hobbiesBtnElement.style.backgroundColor = "rgb(244, 124, 36)";
+           comidaBtnElement.style.backgroundColor = "rgb(90, 3, 2)";
+           break;
+       case "paseos":
+           hobbiesElement.style.display = "none";
+           comidaElement.style.display = "none";
+           paseosElement.style.display = "block";
+        
+           comidaBtnElement.style.backgroundColor = "rgb(244, 124, 36)";
+           hobbiesBtnElement.style.backgroundColor = "rgb(244, 124, 36)";
+           paseosBtnElement.style.backgroundColor = "rgb(90, 3, 2)";
+           break;
+        default:
+            comidaElement.style.display = "none";
+            paseosElement.style.display = "none";
+            hobbiesElement.style.display = "block";"rgb(90, 3, 2)";
 
-          comidaBtnCategory.style.backgroundColor = "#f47c24";
-          paseosBtnCategory.style.backgroundColor = "#f47c24";
-          hobbiesBtnCategory.style.backgroundColor = "#5a0302";
-          break;
-        default :
-          paseosCategory.style.display = "none";
-          hobbiesCategory.style.display = "none";
-          comidaCategory.style.display = "block";
+            paseosBtnElement.style.backgroundColor = "rgb(244, 124, 36)";
+            comidaBtnElement.style.backgroundColor = "rgb(244, 124, 36)";
+            hobbiesBtnElement.style.backgroundColor = "rgb(90, 3, 2)";
+            break;
 
-          paseosBtnCategory.style.backgroundColor = "#f47c24";
-          hobbiesBtnCategory.style.backgroundColor = "#f47c24";
-          comidaBtnCategory.style.backgroundColor = "#5a0302";
-          break;
-    }       
+
+    }
+
 
 }
